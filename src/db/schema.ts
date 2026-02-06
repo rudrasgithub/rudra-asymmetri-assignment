b324785ddde4131514a7cf16baa7bc6d7bebc392
rudrasgithub <rudramanaidu99@gmail.com>
2026-02-06 17:54:49 +0530

    Setup drizzle ORM and config

---
+++ src/db/schema.ts
@@ -0,0 +1,75 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+//   uuid,
+//   primaryKey,
+//   integer,
+//   jsonb,
+} from "drizzle-orm/pg-core";
+// import { type AdapterAccount } from "next-auth/adapters";
+
+// authe and User Table schemas
+export const users = pgTable("user", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: text("name"),
+  email: text("email").notNull(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
+});
+
+// export const accounts = pgTable(
+//   "account",
+//   {
+//     userId: text("userId")
+//       .notNull()
+//       .references(() => users.id, { onDelete: "cascade" }),
+//     type: text("type").$type<AdapterAccount["type"]>().notNull(),
+//     provider: text("provider").notNull(),
+//     providerAccountId: text("providerAccountId").notNull(),
+//     refresh_token: text("refresh_token"),
+//     access_token: text("access_token"),
+//     expires_at: integer("expires_at"),
+//     token_type: text("token_type"),
+//     scope: text("scope"),
+//     id_token: text("id_token"),
+//     session_state: text("session_state"),
+//   },
+//   (account) => ({
+//     compoundKey: primaryKey({
+//       columns: [account.provider, account.providerAccountId],
+//     }),
+//   })
+// );
+
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+});
+
+// chat and Message schemas
+// export const chats = pgTable("chat", {
+//   id: uuid("id").defaultRandom().primaryKey(),
+//   userId: text("userId")
+//     .notNull()
+//     .references(() => users.id, { onDelete: "cascade" }), // Links chat to a user
+//   title: text("title").notNull(), 
+//   createdAt: timestamp("created_at").defaultNow().notNull(),
+// });
+
+
+// export const messages = pgTable("message", {
+//   id: uuid("id").defaultRandom().primaryKey(),
+//   chatId: uuid("chatId")
+//     .notNull()
+//     .references(() => chats.id, { onDelete: "cascade" }),
+//   role: text("role", { enum: ["user", "assistant", "system", "data", "tool"] }).notNull(),
+//   content: text("content"),
+//   toolInvocations: jsonb("tool_invocations"), 
+//   createdAt: timestamp("created_at").defaultNow().notNull(),
+// });
